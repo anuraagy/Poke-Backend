e56d94ac41424654d68e46b530621d48df2e5c98
bienzaaron <ajbienz@purdue.edu>
2019-03-04 18:28:29 -0500

    fix rating

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,12 +2,12 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :bio, :phone_number, :rating, :created_at, :updated_at, :rating
 
   def rating
-    count = Reminder.where(creator: self).where.not(creator_rating: nil).count +
-            Reminder.where(caller: self).where.not(caller_rating: nil).count
+    count = Reminder.where(creator: object).where.not(creator_rating: nil).count +
+            Reminder.where(caller: object).where.not(caller_rating: nil).count
     return 0 if count == 0
-    sum = Reminder.where(creator: self).where.not(creator_rating: nil).sum(:creator_rating) +
-          Reminder.where(caller: self).where.not(caller_rating: nil).sum(:caller_rating)
-    sum / count
+    sum = Reminder.where(creator: object).where.not(creator_rating: nil).sum(:creator_rating) +
+          Reminder.where(caller: object).where.not(caller_rating: nil).sum(:caller_rating)
+    sum.to_f / count.to_f
   end
 end
    
