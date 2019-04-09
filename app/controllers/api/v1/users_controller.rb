97ea5f84e3e39fea25b9257832d872f12fc2b70a
AJ Bienz <ajbienz@purdue.edu>
2019-04-09 16:26:50 -0400

    add activity to user profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     profile['active_reminders'] = Reminder.where(creator: current_user).where.not(status: 'triggered').count
     profile['times_reminded_others'] = Reminder.where(caller: current_user).count
     profile['next_reminder'] = Reminder.where(creator: current_user).where.not(status: 'triggered').order('will_trigger_at').first
-    # user activity goes here next sprint
+    profile['activity'] = current_user.activity
     render status: :ok, json: { profile: profile }
   end
 
@@ -25,8 +25,7 @@ class Api::V1::UsersController < Api::V1::BaseController
           profile['reminder_count'] = Reminder.where(creator: user).count
           profile['active_reminders'] = Reminder.where(creator: user).where.not(status: 'triggered').count
           profile['times_reminded_others'] = Reminder.where(caller: user).count
-          # user activity goes here next sprint
-          # limit activity based on friends, etc
+          profile['activity'] = user.activity
           render status: :ok, json: { profile: profile }
         else
           render status: :bad_request, json: { errors: ["User not found"] }
@@ -42,8 +41,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       profile['reminder_count'] = Reminder.where(creator: user).count
       profile['active_reminders'] = Reminder.where(creator: user).where.not(status: 'triggered').count
       profile['times_reminded_others'] = Reminder.where(caller: user).count
-      # user activity goes here next sprint
-      # limit activity based on friends, etc
+      profile['activity'] = user.activity
       render status: :ok, json: { profile: profile }
     else
       render status: :bad_request, json: { errors: ["User not found"] }
